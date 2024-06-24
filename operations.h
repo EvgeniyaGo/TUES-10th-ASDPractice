ea4fdb13f1c4c9fb577a3a66176d3ff5ff94df77
Adr1ana79 <117771916+Adr1ana79@users.noreply.github.com>
2024-06-24 14:58:50 +0300

    Update operations.h
    Подреждане на файла и добавяне на функция

--- operations.h
+++ operations.h
@@ -4,6 +4,7 @@
 #define COUNTRY_CODE "BG"
 #define BANK_CODE "BNBG"
 
+// Accounts
 typedef struct bank_account {
     char iban[30];
     double balance;
@@ -14,11 +15,13 @@ void generate_random_account_number(char *account_number, int length);
 int calculate_checksum(const char *iban);
 void generate_iban(char *iban);
 
-void deposit(bank_account *b_account, double amount);
-void withdrawals(bank_account *b_account, double amount);
+bank_account * findAccountByIDFromFile(char * filename, char * ID);
+bank_account * registerBankAccount(char *ID, char * filename);
+void fileAddBankAccount(bank_account *bAccount, char *filename);
+bank_account * createBankAccount(char * ID);
+void updatefileById(char *filename, char * searchindId, bank_account * bAccount);
 
 // Transactions
-
 typedef struct transaction {
     char status;
     char key[20];
@@ -42,14 +45,12 @@ void enqueue(transaction_queue *queue, transaction *new_trans);
 void create_transaction(HashTable *table, transaction_queue *queue, double amount, bank_account *sender, bank_account *receiver);
 void free_queue(transaction_queue *queue);
 
-void transaction_func(transaction_queue *queue, bank_account *sender, bank_account *receiver, transaction *trans);
-void save_transaction(transaction_queue *queue);
-void transaction_processing(HashTable *table, transaction_queue *queue);
+// Operations
+void deposit(bank_account *b_account, double amount);
+void withdrawals(bank_account *b_account, double amount);
 
-bank_account * findAccountByIDFromFile(char * filename, char * ID);
-bank_account * registerBankAccount(char *ID, char * filename);
-void fileAddBankAccount(bank_account *bAccount, char *filename);
-bank_account * createBankAccount(char * ID);
-void updatefileById(char *filename, char * searchindId, bank_account * bAccount);
+void transaction_func(transaction_queue *queue, bank_account *sender, bank_account *receiver, transaction *trans);
+void save_transactions(transaction_queue *queue);
+void transaction_processing(HashTable *table, transaction_queue *queue);
 
 #endif
