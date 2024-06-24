54ed22ba6ea5e0a1473e50c8a74257220a8d6bac
Adr1ana79 <117771916+Adr1ana79@users.noreply.github.com>
2024-06-24 15:02:40 +0300

    Update transaction_queue.h

--- transaction_queue.h
+++ transaction_queue.h
@@ -6,9 +6,12 @@
 #define TRANS_H
 
 void init_queue(transaction_queue *queue);
-void free_queue(transaction_queue *queue);
+
 void create_transaction(transaction_queue *queue, double amount, bank_account *sender, bank_account *receiver);
 void enqueue(transaction_queue *queue, transaction *new_trans);
+void generate_transaction_code(char *buffer, size_t buffer_size);
+
+void free_queue(transaction_queue *queue);
 
 
 #endif 
